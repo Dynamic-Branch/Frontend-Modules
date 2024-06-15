8cbf1df73051445fa84dcef825bb4f6be4e7f1cc
ClaudiuIordache <claudiuiordache@outlook.com>
2024-06-15 13:53:23 +0300

    added signature option

--- frontend-modules/src/App.jsx
+++ frontend-modules/src/App.jsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import SignatureCanvas from 'react-signature-canvas';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Frontend Modules</h1>
+      <h4>Sign here:</h4>
+      <SignatureCanvas
+        className='signature'
+        penColor='green'
+        canvasProps={{
+          width: 500,
+          height: 200,
+          className: 'sigCanvas',
+        }}
+      />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
