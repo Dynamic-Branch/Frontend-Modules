e984df81b56980aba543f33cb270d94b9d8e6725
ClaudiuIordache <claudiuiordache@outlook.com>
2024-06-22 15:41:20 +0300

    added text and buttons

--- frontend-modules/src/App.jsx
+++ frontend-modules/src/App.jsx
@@ -1,19 +1,86 @@
 import './App.css';
 import SignatureCanvas from 'react-signature-canvas';
+import mockData from './mock-data';
+import { useState, useRef } from 'react';
 
 function App() {
+  const [signatureMode, setSignatureMode] = useState(false);
+  const [trimmedCanvas, setTrimmedCanvas] = useState('');
+  const signaturePadRef = useRef(null);
+
+  function handleSignatureClick() {
+    return setSignatureMode(!signatureMode);
+  }
+
+  function clearPad() {
+    if (signaturePadRef.current) {
+      signaturePadRef.current.clear();
+    }
+  }
+
+  function trimPad() {
+    if (signaturePadRef.current) {
+      setTrimmedCanvas(
+        signaturePadRef.current.getTrimmedCanvas().toDataURL('image/png')
+      );
+    }
+  }
   return (
     <>
-      <h1>Frontend Modules</h1>
-      <h4>Sign here:</h4>
-      <SignatureCanvas
-        penColor='green'
-        canvasProps={{
-          width: 500,
-          height: 200,
-          className: 'sigCanvas',
-        }}
-      />
+      <h3>Formular înscriere</h3>
+      <div>{mockData}</div>
+      {!signatureMode ? (
+        <button className={'button'} onClick={handleSignatureClick}>
+          Accept and sign
+        </button>
+      ) : null}
+
+      {signatureMode ? (
+        <form className='form'>
+          <div className='formFirstRow'>
+            <SignatureCanvas
+              ref={signaturePadRef}
+              penColor='green'
+              canvasProps={{
+                width: 500,
+                height: 200,
+                className: 'sigCanvas',
+              }}
+            />
+          </div>
+          <div className='formSecondRow'>
+            <button
+              className={'button'}
+              type='button'
+              onClick={handleSignatureClick}
+            >
+              Go back
+            </button>
+            <button
+              className={'button'}
+              id='buttonClear'
+              type='button'
+              onClick={clearPad}
+            >
+              Clear
+            </button>
+            <button
+              className={'button'}
+              id='buttonTrim'
+              type='button'
+              onClick={trimPad}
+            >
+              Trim
+            </button>
+            <button className={'button'} id='buttonProceed' type='button'>
+              Proceed to checkout
+            </button>
+          </div>
+        </form>
+      ) : null}
+      <div>
+        <img src={trimmedCanvas} />
+      </div>
     </>
   );
 }
